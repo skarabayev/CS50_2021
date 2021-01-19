b4db7b93b110582e3f54e82cac14ea5ef2ac221f
Sabit Karabayev <62263769+skarabayev@users.noreply.github.com>
2021-01-19 13:21:19 +0500

    Update mario.py

--- mario.py
+++ mario.py
@@ -1,1 +1,14 @@
+def main():
+    size = int(input("Enter size of the pyramid: "))
+    constructor(size)
 
+def constructor(size):
+    spaces = size - 1
+    for i in range(0, size):
+        for j in range(0,spaces):
+            print(" ", end="")
+        spaces -=1
+        for j in range(0, i+1):
+            print("#", end="")
+        print()
+main()
